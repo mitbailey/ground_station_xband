59f3790764ff46baa9d451936eae2e92c3f277bb
Mit Bailey <mitbailey99@gmail.com>
2021-08-08 00:37:43 -0400

    Removed phy config t

--- include/gs_xband.hpp
+++ include/gs_xband.hpp
@@ -33,25 +33,6 @@ typedef struct
 } global_data_t;
 
 /**
- * @brief For configuring Roof X-Band / Haystack.
- * 
- */
-typedef struct
-{
-    int mode;               // SLEEP, FDD, TDD
-    int pll_freq;           // PLL Frequency
-    uint64_t LO;            // LO freq
-    uint64_t samp;          // sampling rate
-    uint64_t bw;            // bandwidth
-    char ftr_name[64];      // filter name
-    int temp;               // temperature
-    double rssi;            // RSSI
-    double gain;            // TX Gain
-    char curr_gainmode[16]; // fast_attack or slow_attack
-    bool pll_lock;
-} phy_config_t;
-
-/**
  * @brief X-Band data structure.
  * 
  * From line 113 of https://github.com/SPACE-HAUC/shflight/blob/flight_test/src/cmd_parser.c
